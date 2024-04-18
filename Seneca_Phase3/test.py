dc2dc49651fec64d1737855fa3f15310bd8ff712
Maestro-111 <donalddack112@gmail.com>
2024-04-19 01:14:54 +0300

    param tweak in test.py

--- Seneca_Phase3/test.py
+++ Seneca_Phase3/test.py
@@ -113,8 +113,8 @@ def str2bool(v):
 
 parser = argparse.ArgumentParser(description='CRAFT Text Detection')
 parser.add_argument('--trained_model', default='weights/craft_mlt_25k.pth', type=str, help='pretrained model')
-parser.add_argument('--text_threshold', default=0.05, type=float, help='text confidence threshold')
-parser.add_argument('--low_text', default=0.05, type=float, help='text low-bound score')
+parser.add_argument('--text_threshold', default=0.4, type=float, help='text confidence threshold')
+parser.add_argument('--low_text', default=0.1, type=float, help='text low-bound score')
 parser.add_argument('--link_threshold', default=0.5, type=float, help='link confidence threshold')
 parser.add_argument('--cuda', default=False, type=str2bool, help='Use cuda for inference')
 parser.add_argument('--canvas_size', default=1280, type=int, help='image size for inference')
